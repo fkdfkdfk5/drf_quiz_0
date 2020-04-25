2a6e4aaab229afb1f8ef1f9a3954afa0b1f366d5
fkdfkdfk5 <fkdfkdfk5@gmail.com>
2020-04-25 16:43:52 +0900

    Ready for deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn quiz_0.wsgi
---
+++ quiz/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Quiz
+
+
+class QuizSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Quiz
+        fields = ('title', 'body', 'answer')
